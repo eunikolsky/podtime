03a46886b1225067c7db5a4a677df55783190ff7
Eugene <e@egeek.me>
2020-10-01 21:05:21 +0300

    Print all podcast identifiers from gPodder

--- podtime/src/Main.hs
+++ podtime/src/Main.hs
@@ -1,5 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.List (intercalate)
+import Database.SQLite.Simple
+import System.Directory (getHomeDirectory)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  podcasts <- getPodcasts
+  putStrLn . intercalate ", " . fmap show $ podcasts
+
+-- | Returns a list of all podcasts in gPodder. Assumes the database
+-- at the default location `~/gPodder/Database`.
+getPodcasts :: IO [Int]
+getPodcasts = do
+  homeDir <- getHomeDirectory
+  withConnection (homeDir ++ "/gPodder/Database") $ \conn -> do
+    ids <- query_ conn "SELECT id FROM podcast" :: IO [Only Int]
+    return $ fromOnly <$> ids
