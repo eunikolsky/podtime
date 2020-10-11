43942fa38679ca0a1dac231806c5cf4e72359bd3
Eugene <e@egeek.me>
2020-10-11 22:12:08 +0300

    Start parsing MP3 frames

---
+++ src/MP3.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module MP3 where
+
+import qualified Data.ByteString.Lazy as BL
+import           Data.Functor
+import           Data.Void
+import           Text.Megaparsec
+import           Text.Megaparsec.Byte
+
+type Parser = Parsec Void BL.ByteString
+
+type ParserBL = Parser BL.ByteString
+
+type Frame = ()
+
+-- | Parses the start of an MP3 frame header, assuming MPEG-1 Layer 3
+-- without error protection.
+headerStart :: ParserBL
+headerStart = string "\xff\xfb"
+
+-- | Parses an MP3 stream.
+mp3Parser :: Parser [Frame]
+mp3Parser = optionalID3 $> headerStart $> [()]
+  where
+    optionalID3 = do
+      void $ string "ID3"
+      void $ many anySingle
+
+newtype BitRate = BitRate { unBitRate :: Int }
+newtype SampleRate = SampleRate { unSampleRate :: Int }
+newtype Padding = Padding { unPadding :: Bool }
+
+-- http://www.multiweb.cz/twoinches/MP3inside.htm
+
+-- | @frameLen@ includes frame header (4 bytes).
+frameLen :: BitRate -> SampleRate -> Padding -> Int
+frameLen br sr p = floor $ (144.0 * (fromIntegral (unBitRate br) :: Double) / (fromIntegral (unSampleRate sr) :: Double)) + padding
+  where padding = if unPadding p then 1 else 0
