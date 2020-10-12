282470a0e5cde76d2d4c73afd021499b72f62a15
Eugene <e@egeek.me>
2020-10-12 21:50:26 +0300

    Parse all MP3 frames

--- src/MP3.hs
+++ src/MP3.hs
@@ -2,9 +2,11 @@
 
 module MP3 where
 
+import           Data.Bits
 import qualified Data.ByteString.Lazy as BL
 import           Data.Functor
 import           Data.Void
+import           Data.Word
 import           Text.Megaparsec
 import           Text.Megaparsec.Byte
 
@@ -14,16 +16,38 @@ type ParserBL = Parser BL.ByteString
 
 type Frame = ()
 
--- | Parses the start of an MP3 frame header, assuming MPEG-1 Layer 3
+-- | Parses an MP3 frame header, assuming MPEG-1 Layer 3 and
 -- without error protection.
-headerStart :: ParserBL
-headerStart = string "\xff\xfb"
+mp3Frame :: Parser Frame
+mp3Frame = do
+  void $ string "\xff\xfb"
+  details <- anySingle
+  void anySingle
+
+  skipCount (restLength details) anySingle
+  return ()
+
+  where
+    restLength :: Word8 -> Int
+    restLength details =
+      let br = BitRate . (bitRates !!) . (.&. 0x0f) . (`shiftR` 4) . fromIntegral $ details
+          sr = SampleRate . (sampleRates !!) . (.&. 0x03) . (`shiftR` 2) . fromIntegral $ details
+          p = Padding $ testBit details 1
+
+          bitRates =
+            [ undefined, 32000, 40000, 48000
+            , 56000, 64000, 80000, 96000
+            , 112000, 128000, 160000, 192000
+            , 224000, 256000, 320000, undefined
+            ]
+          sampleRates = [44100, 48000, 32000, undefined]
+      in (frameLen br sr p) - 4
 
 -- | Parses an MP3 stream.
 mp3Parser :: Parser [Frame]
 mp3Parser = do
   optional id3Parser
-  headerStart $> [()]
+  some mp3Frame
 
   where
     id3Parser = do
