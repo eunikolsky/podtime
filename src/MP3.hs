95b43ca5f20104b144e34dd5f22e17b917df01a4
Eugene <e@egeek.me>
2023-03-05 08:56:59 +0200

    Support valid sampling rates

--- src/MP3.hs
+++ src/MP3.hs
@@ -2,9 +2,11 @@ module MP3
   ( frameParser
   ) where
 
+import Control.Monad
 import Data.Attoparsec.ByteString ((<?>), Parser)
 import Data.Attoparsec.ByteString qualified as A
 import Data.Bits
+import Data.Word
 
 frameParser :: Parser ()
 frameParser = do
@@ -12,12 +14,31 @@ frameParser = do
   byte2 <- A.anyWord8
   _ <- A.anyWord8
 
+  samplingRate <- samplingRateParser byte2
   let paddingSize = if testBit byte2 paddingBitIndex then 1 else 0
-      frameSize = 417
-      contentsSize = frameSize - 4 + paddingSize
+      contentsSize = frameSize samplingRate - 4 + paddingSize
 
   _ <- A.take contentsSize
   pure ()
+
+-- | Sampling rate of a frame; it's required to calculate the frame length.
+data SamplingRate = SR32000Hz | SR44100Hz | SR48000Hz
+
+-- | Parses the sample rate from the frame byte.
+samplingRateParser :: Word8 -> Parser SamplingRate
+samplingRateParser byte = case 0b00000011 .&. shiftR byte 2 of
+  0b00 -> pure SR44100Hz
+  0b01 -> pure SR48000Hz
+  0b10 -> pure SR32000Hz
+  _ -> mzero -- FIXME a better error message?
+
+-- | Returns the frame length based on 128 kb/s bitrate and the provided sample rate.
+frameSize :: SamplingRate -> Int
+frameSize sr = floor @Float $ 144 * 128000 / samplingRateHz sr
+  where
+    samplingRateHz SR32000Hz = 32000
+    samplingRateHz SR44100Hz = 44100
+    samplingRateHz SR48000Hz = 48000
 
 -- TODO try https://github.com/stevana/bits-and-bobs
 paddingBitIndex :: Int
--- test/MP3Spec.hs
+++ test/MP3Spec.hs
@@ -35,6 +35,11 @@ spec = parallel $ do
         . forAll genFrameWithPadding $ \frame ->
           complete frameParser `shouldSucceedOn` frame
 
+      forM_ (enumFromTo minBound maxBound) $ \samplingRate ->
+        prop ("parses a basic 128 kbps frame with sampling rate " <> show samplingRate) .
+          forAll (genFrameWithSamplingRate samplingRate) $ \frame ->
+            complete frameParser `shouldSucceedOn` frame
+
       prop "fails to parse bytes with incorrect first byte"
         . forAll genInvalidFrame $ \frame ->
           frameParser `shouldFailOn` frame
@@ -50,18 +55,38 @@ contentsSize = frameSize - frameHeaderSize
 
 newtype PaddingBit = PaddingBit Bool
 
-mkHeader :: PaddingBit -> ByteString
-mkHeader (PaddingBit paddingBitSet) = BS.pack
+data SamplingRate = SR44100 | SR48000 | SR32000
+  deriving stock (Bounded, Enum)
+
+instance Show SamplingRate where
+  show SR44100 = "44.1 kHz"
+  show SR48000 = "48 kHz"
+  show SR32000 = "32 kHz"
+
+mkHeader :: PaddingBit -> SamplingRate -> ByteString
+mkHeader (PaddingBit paddingBitSet) sr = BS.pack
   [ 0xff
   , 0b11111011
-  , (if paddingBitSet then setBit else clearBit) 0b10010000 paddingBitIndex
+  , byte2
   , 0b11000100
   ]
 
-  where paddingBitIndex = 1
+  where
+    paddingBitIndex = 1
+    byte2 =
+      (.|. samplingRateByte sr)
+      . flip (if paddingBitSet then setBit else clearBit) paddingBitIndex
+      $ 0b10010000
+
+-- | Returns a zeroed frame byte where only the sampling rate bits are set
+-- corresponding to `sr`.
+samplingRateByte :: SamplingRate -> Word8
+samplingRateByte SR44100 = zeroBits
+samplingRateByte SR48000 = 0b00000100
+samplingRateByte SR32000 = 0b00001000
 
 header :: ByteString
-header = mkHeader (PaddingBit False)
+header = mkHeader (PaddingBit False) SR44100
 
 -- | A standard 128 kb/s, 44.1 kHz mp3 frame.
 mkFrame :: ByteString
@@ -79,7 +104,18 @@ genFrame = do
 genFrameWithPadding :: Gen ByteString
 genFrameWithPadding = do
   contents <- vectorOf (contentsSize + 1) arbitrary
-  pure $ mkHeader (PaddingBit True) <> BS.pack contents
+  pure $ mkHeader (PaddingBit True) SR44100 <> BS.pack contents
+
+-- | Generates a standard 128 kb/s mp3 frame with the given sampling rate and
+-- arbitrary contents.
+genFrameWithSamplingRate :: SamplingRate -> Gen ByteString
+genFrameWithSamplingRate sr = do
+  let contentsSize' = case sr of
+        SR44100 -> 417
+        SR48000 -> 384
+        SR32000 -> 576
+  contents <- vectorOf (contentsSize' - frameHeaderSize) arbitrary
+  pure $ mkHeader (PaddingBit False) sr <> BS.pack contents
 
 -- | Generates an invalid mp3 frame where the first byte is incorrect.
 genInvalidFrame :: Gen ByteString
