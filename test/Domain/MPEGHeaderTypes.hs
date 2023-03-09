139e1550d143d2f1a37f72641797f92646144603
Eugene <e@egeek.me>
2023-03-09 21:34:59 +0200

    Move MPEG-specific definitions into `Domain.MPEGHeaderTypes`

---
+++ test/Domain/MPEGHeaderTypes.hs
@@ -0,0 +1,105 @@
+module Domain.MPEGHeaderTypes
+  ( Layer(..)
+  , MPEGSettings(..)
+  , MPEGVersion(..)
+  , Protection(..)
+  , mkHeader
+  , mkMPEGHeader
+  ) where
+
+import Data.Bits
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.Foldable
+import Data.Word
+import Domain.FrameSync
+import Domain.MP3HeaderTypes
+
+data MPEGVersion = MPEG1 | MPEG2 | MPEG25 | MPEGReserved
+
+instance Show MPEGVersion where
+  show MPEG1 = "MPEG Version 1"
+  show MPEG2 = "MPEG Version 2"
+  show MPEG25 = "MPEG Version 2.5"
+  show MPEGReserved = "MPEG Version <reserved>"
+
+data Layer = Layer1 | Layer2 | Layer3 | LayerReserved
+  deriving stock (Bounded, Enum)
+
+instance Show Layer where
+  show Layer1 = "Layer I"
+  show Layer2 = "Layer II"
+  show Layer3 = "Layer III"
+  show LayerReserved = "Layer <reserved>"
+
+data MPEGSettings = MP3 !MP3FrameSettings | MPEGOther !MPEGVersion !Layer
+
+instance Show MPEGSettings where
+  show (MP3 _) = "MP3"
+  show (MPEGOther v l) = mconcat [show v, ", ", show l]
+
+mpegVersion :: MPEGSettings -> MPEGVersion
+mpegVersion (MP3 _) = MPEG1
+mpegVersion (MPEGOther v _) = v
+
+mpegLayer :: MPEGSettings -> Layer
+mpegLayer (MP3 _) = Layer3
+mpegLayer (MPEGOther _ l) = l
+
+data Protection = NotProtected | ProtectedCRC
+
+-- | Returns a zeroed frame byte where only the MPEG version bits are set
+-- corresponding to `MPEGVersion`.
+mpegVersionByte :: MPEGVersion -> Word8
+mpegVersionByte MPEG1        = 0b00011000
+mpegVersionByte MPEG2        = 0b00010000
+mpegVersionByte MPEG25       = 0b00000000
+mpegVersionByte MPEGReserved = 0b00001000
+
+-- | Returns a zeroed frame byte where only the Layer number bits are set
+-- corresponding to `Layer`.
+layerByte :: Layer -> Word8
+layerByte Layer1        = 0b110
+layerByte Layer2        = 0b100
+layerByte Layer3        = 0b010
+layerByte LayerReserved = 0b000
+
+-- | Returns a zeroed frame byte where only the Protection bit is set
+-- corresponding to `Protection`.
+protectionByte :: Protection -> Word8
+protectionByte NotProtected = 1
+protectionByte ProtectedCRC = 0
+
+-- | Returns data for MP3 header with the given settings.
+mkHeader :: MP3FrameSettings -> ByteString
+mkHeader = mkMPEGHeader validFrameSync NotProtected . MP3
+
+-- | Returns data for an MPEG header with the given settings.
+mkMPEGHeader :: FrameSync -> Protection -> MPEGSettings -> ByteString
+-- TODO use Data.Binary.Put ?
+mkMPEGHeader frameSync protection mpeg = BS.pack
+  [ byte0
+  , byte1
+  , byte2
+  , 0b11000100
+  ]
+
+  where
+    (byte0, initialByte1) = frameSyncBytes frameSync
+    byte1 = orBytes
+      [ mpegVersionByte (mpegVersion mpeg)
+      , layerByte (mpegLayer mpeg)
+      , initialByte1
+      , protectionByte protection
+      ]
+    byte2 = case mpeg of
+      MP3 mp3Settings -> orBytes
+        [ bitrateByte $ mfBitrate mp3Settings
+        , samplingRateByte $ mfSamplingRate mp3Settings
+        , paddingByte $ mfPadding mp3Settings
+        ]
+      _ -> 0
+
+-- | `OR`s all the bytes in the container.
+orBytes :: (Bits a, Foldable t) => t a -> a
+orBytes = getIor . foldMap' Ior
--- test/MP3Spec.hs
+++ test/MP3Spec.hs
@@ -3,14 +3,12 @@ module MP3Spec (spec) where
 import Control.Monad
 import Data.Attoparsec.ByteString (Parser)
 import Data.Attoparsec.ByteString qualified as A
-import Data.Bits
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
-import Data.Foldable
 import Data.Maybe
-import Data.Word
 import Domain.FrameSync
 import Domain.MP3HeaderTypes
+import Domain.MPEGHeaderTypes
 import MP3
 import Prelude hiding (pred)
 import Test.Hspec
@@ -92,95 +90,6 @@ Right parsed `shouldFailWithErrorContaining` _ = expectationFailure $ "Unexpecte
 -- | Parser combinator to make sure the entire input is consumed.
 complete :: Parser a -> Parser a
 complete = (<* A.endOfInput)
-
-data MPEGVersion = MPEG1 | MPEG2 | MPEG25 | MPEGReserved
-
-instance Show MPEGVersion where
-  show MPEG1 = "MPEG Version 1"
-  show MPEG2 = "MPEG Version 2"
-  show MPEG25 = "MPEG Version 2.5"
-  show MPEGReserved = "MPEG Version <reserved>"
-
-data Layer = Layer1 | Layer2 | Layer3 | LayerReserved
-  deriving stock (Bounded, Enum)
-
-instance Show Layer where
-  show Layer1 = "Layer I"
-  show Layer2 = "Layer II"
-  show Layer3 = "Layer III"
-  show LayerReserved = "Layer <reserved>"
-
-data MPEGSettings = MP3 !MP3FrameSettings | MPEGOther !MPEGVersion !Layer
-
-instance Show MPEGSettings where
-  show (MP3 _) = "MP3"
-  show (MPEGOther v l) = mconcat [show v, ", ", show l]
-
-mpegVersion :: MPEGSettings -> MPEGVersion
-mpegVersion (MP3 _) = MPEG1
-mpegVersion (MPEGOther v _) = v
-
-mpegLayer :: MPEGSettings -> Layer
-mpegLayer (MP3 _) = Layer3
-mpegLayer (MPEGOther _ l) = l
-
-data Protection = NotProtected | ProtectedCRC
-
--- | Returns data for an MPEG header with the given settings.
-mkMPEGHeader :: FrameSync -> Protection -> MPEGSettings -> ByteString
--- TODO use Data.Binary.Put ?
-mkMPEGHeader frameSync protection mpeg = BS.pack
-  [ byte0
-  , byte1
-  , byte2
-  , 0b11000100
-  ]
-
-  where
-    (byte0, initialByte1) = frameSyncBytes frameSync
-    byte1 = orBytes
-      [ mpegVersionByte (mpegVersion mpeg)
-      , layerByte (mpegLayer mpeg)
-      , initialByte1
-      , protectionByte protection
-      ]
-    byte2 = case mpeg of
-      MP3 mp3Settings -> orBytes
-        [ bitrateByte $ mfBitrate mp3Settings
-        , samplingRateByte $ mfSamplingRate mp3Settings
-        , paddingByte $ mfPadding mp3Settings
-        ]
-      _ -> 0
-
--- | `OR`s all the bytes in the container.
-orBytes :: (Bits a, Foldable t) => t a -> a
-orBytes = getIor . foldMap' Ior
-
--- | Returns data for MP3 header with the given settings.
-mkHeader :: MP3FrameSettings -> ByteString
-mkHeader = mkMPEGHeader validFrameSync NotProtected . MP3
-
--- | Returns a zeroed frame byte where only the MPEG version bits are set
--- corresponding to `MPEGVersion`.
-mpegVersionByte :: MPEGVersion -> Word8
-mpegVersionByte MPEG1        = 0b00011000
-mpegVersionByte MPEG2        = 0b00010000
-mpegVersionByte MPEG25       = 0b00000000
-mpegVersionByte MPEGReserved = 0b00001000
-
--- | Returns a zeroed frame byte where only the Layer number bits are set
--- corresponding to `Layer`.
-layerByte :: Layer -> Word8
-layerByte Layer1        = 0b110
-layerByte Layer2        = 0b100
-layerByte Layer3        = 0b010
-layerByte LayerReserved = 0b000
-
--- | Returns a zeroed frame byte where only the Protection bit is set
--- corresponding to `Protection`.
-protectionByte :: Protection -> Word8
-protectionByte NotProtected = 1
-protectionByte ProtectedCRC = 0
 
 standardMP3Settings :: MP3FrameSettings
 standardMP3Settings = MP3FrameSettings (BRValid VBV128) SR44100 NoPadding
