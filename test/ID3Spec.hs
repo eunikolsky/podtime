850dd7ebb31276f7da0aa5e3054f01b02066156e
Eugene <e@egeek.me>
2023-03-13 13:03:19 +0200

    Make amended tag settings in tests more explicit
    For example, now it's much clearer that a test uses a non-default
    identifier or version.

--- test/ID3Spec.hs
+++ test/ID3Spec.hs
@@ -28,14 +28,14 @@ spec = parallel $ do
         id3Parser `shouldFailOn` tag
 
 sampleID3Tag :: ByteString
-sampleID3Tag = mkID3Tag $ ID3TagSettings "ID3" "\x04\x00" 0
+sampleID3Tag = mkID3Tag defaultID3TagSettings
 
 -- | Generates an ID3 v2.4 tag where the identifier "ID3" is replaced with arbitrary bytes.
 genTagWithInvalidID3 :: Gen ByteString
 genTagWithInvalidID3 = do
   -- it's unlikely that this will generate "ID3"
   identifier <- vectorOf 3 arbitrary
-  pure . mkID3Tag $ ID3TagSettings (BS.pack identifier) "\x04\x00" 0
+  pure . mkID3Tag $ defaultID3TagSettings { idsIdentifier = BS.pack identifier }
 
 -- | Generates an ID3 tag with an arbitrary, non-2.4 version.
 genTagWithUnsupportedVersion :: Gen ByteString
@@ -43,19 +43,22 @@ genTagWithUnsupportedVersion = do
   version <- vectorOf 2 arbitrary
   if version == [4, 0]
     then discard
-    else pure . mkID3Tag $ ID3TagSettings "ID3" (BS.pack version) 0
+    else pure . mkID3Tag $ defaultID3TagSettings { idsVersion = BS.pack version }
 
 -- | Generates an ID3 tag with an arbitrary byte for flags.
 genTagWithFlags :: Gen ByteString
 genTagWithFlags = do
   flags <- getPositive <$> arbitrary
-  pure . mkID3Tag $ ID3TagSettings "ID3" "\x04\x00" flags
+  pure . mkID3Tag $ defaultID3TagSettings { idsFlags = flags }
 
 data ID3TagSettings = ID3TagSettings
   { idsIdentifier :: ByteString
   , idsVersion :: ByteString
   , idsFlags :: Word8
   }
+
+defaultID3TagSettings :: ID3TagSettings
+defaultID3TagSettings = ID3TagSettings "ID3" "\x04\x00" 0
 
 mkID3Tag :: ID3TagSettings -> ByteString
 mkID3Tag ids = mconcat [idsIdentifier ids, idsVersion ids, flags, size, contents]
