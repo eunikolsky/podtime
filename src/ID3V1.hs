d38b6e846c95d79f7924faed4a8e61ec0aef46a7
Eugene <e@egeek.me>
2023-03-18 10:41:42 +0200

    Fail parsing on invalid tag identifier

--- src/ID3V1.hs
+++ src/ID3V1.hs
@@ -2,8 +2,10 @@ module ID3V1
   ( id3Parser
   ) where
 
+import Control.Monad
 import Data.Attoparsec.ByteString (Parser)
+import Data.Attoparsec.ByteString qualified as A
 
 -- | Parses the ID3 v1 tag.
 id3Parser :: Parser ()
-id3Parser = pure ()
+id3Parser = void $ A.string "TAG"
--- test/ID3V1Spec.hs
+++ test/ID3V1Spec.hs
@@ -5,6 +5,8 @@ import Data.ByteString qualified as BS
 import ID3V1
 import Test.Hspec
 import Test.Hspec.Attoparsec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 spec :: Spec
 spec = parallel $ do
@@ -12,5 +14,19 @@ spec = parallel $ do
     it "parses ID3 v1 tag" $
       id3Parser `shouldSucceedOn` sampleTag
 
+    prop "fails to parse tag with invalid identifier" $ \(InvalidTag tag) ->
+      id3Parser `shouldFailOn` tag
+
 sampleTag :: ByteString
-sampleTag = "TAG" <> BS.replicate (128 - 3) 0
+sampleTag = "TAG" <> BS.replicate (tagSize - 3) 0
+
+tagSize :: Int
+tagSize = 128
+
+-- | An invalid ID3 v1 tag with 128 arbitrary bytes.
+newtype InvalidTag = InvalidTag ByteString
+  deriving stock (Show)
+
+instance Arbitrary InvalidTag where
+  -- it's unlikely that the bytestring will start with "TAG"
+  arbitrary = InvalidTag . BS.pack <$> vectorOf tagSize arbitrary
