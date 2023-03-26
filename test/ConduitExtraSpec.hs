3bd115e151ef2e604406f7ffc92888dda3532880
Eugene <e@egeek.me>
2023-03-26 17:24:51 +0300

    Test that all elements, fewer than the maximum, are returned

--- test/ConduitExtraSpec.hs
+++ test/ConduitExtraSpec.hs
@@ -14,13 +14,25 @@ spec = parallel $ do
     prop "returns empty list for empty input" $ \n ->
       runConduitPure (sourceNull .| tailC n) `shouldBe` ([] :: [Int])
 
-    prop "returns all elements in order for stream with exactly N elements"
+    prop "returns all elements in order from stream with exactly N elements"
       . forAll genSourceList $ \(n, source) ->
+        runConduitPure (sourceList source .| tailC n) `shouldBe` source
+
+    prop "returns all elements from stream with fewer elements"
+      . forAll genSourceListAtLeastTwoElements $ \(sourceCount, source) -> do
+        let n = sourceCount + 1
         runConduitPure (sourceList source .| tailC n) `shouldBe` source
 
 -- | Generates a non-empty list of arbitrary length and contents.
 genSourceList :: Gen (Word8, [Int])
 genSourceList = do
   n <- getPositive <$> arbitrary
+  list <- vectorOf (fromIntegral n) arbitrary
+  pure (n, list)
+
+-- | Generates an arbitrary list of 2+ elements.
+genSourceListAtLeastTwoElements :: Gen (Word8, [Int])
+genSourceListAtLeastTwoElements = do
+  n <- chooseEnum (2, maxBound - 1)
   list <- vectorOf (fromIntegral n) arbitrary
   pure (n, list)
