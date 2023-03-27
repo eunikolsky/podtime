c1bd27b1cbca0cd9d30f38bd72fcc92f5d1d1446
Eugene <e@egeek.me>
2023-03-27 21:30:53 +0300

    Save the cache only if there were any changes
    Technically, after any inserts.

--- program/Duration.hs
+++ program/Duration.hs
@@ -5,26 +5,34 @@ module Duration
 import Conduit ((.|), MonadIO, MonadThrow, MonadUnliftIO, liftIO, runConduitRes, sourceFile)
 import Control.Concurrent.STM.TVar (TVar, newTVarIO, modifyTVar', readTVarIO)
 import Control.Exception (bracket)
+import Control.Monad (when)
 import Control.Monad.Reader (MonadReader, ReaderT, ask, runReaderT)
 import Control.Monad.STM (atomically)
 import Data.Conduit.Attoparsec (sinkParser)
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M (empty, fromList, insert, lookup, toList)
+import Data.Monoid (Any(..))
 import Data.Text qualified as T (lines, pack, unlines, unpack)
 import Data.Text.IO qualified as T (readFile, writeFile)
 import GetDuration (ModTime, MonadDuration(..), MonadDurationCache(..), MonadModTime(..))
 import MP3 (AudioDuration, mp3Parser)
 import System.Directory (doesFileExist, getModificationTime)
 
--- TODO use text?
-type DurationCacheMap = Map (FilePath, ModTime) AudioDuration
+data DurationCache = DurationCache
+  -- TODO use text?
+  { durationCache :: !(Map (FilePath, ModTime) AudioDuration)
+  -- ^ the cache itself
+  , anyInserts :: !Any
+  -- ^ tracks whether there were any inserts, presumably changing the cache
+  }
 
 -- TODO make it a monad transformer?
 -- FIXME separate file caching from other two tasks of this type?
-newtype CachedDurationM a = CachedDurationM (ReaderT (TVar DurationCacheMap) IO a)
+-- TODO try lock-based var instead of STM
+newtype CachedDurationM a = CachedDurationM (ReaderT (TVar DurationCache) IO a)
   deriving newtype
     ( Functor, Applicative, Monad
-    , MonadReader (TVar DurationCacheMap)
+    , MonadReader (TVar DurationCache)
     , MonadIO, MonadUnliftIO, MonadThrow
     )
 
@@ -38,12 +46,16 @@ instance MonadDuration CachedDurationM where
 instance MonadDurationCache CachedDurationM where
   getCachedDuration key = do
     cacheVar <- ask
-    cache <- liftIO $ readTVarIO cacheVar
-    pure $ M.lookup key cache
+    DurationCache { durationCache } <- liftIO $ readTVarIO cacheVar
+    pure $ M.lookup key durationCache
 
   cacheDuration key duration = do
     cacheVar <- ask
-    liftIO . atomically . modifyTVar' cacheVar $ M.insert key duration
+    liftIO . atomically . modifyTVar' cacheVar $ \DurationCache { durationCache, anyInserts } ->
+      DurationCache
+        { durationCache = M.insert key duration durationCache
+        , anyInserts = anyInserts <> Any True
+        }
 
 instance MonadModTime CachedDurationM where
   getModTime = liftIO . getModificationTime
@@ -55,21 +67,24 @@ withCachedDuration (CachedDurationM a) = bracket loadCache saveCache $ runReader
 -- important: the return type has to be `TVar Map`, not just `Map` because this
 -- value is passed to `saveCache` – if `Map` is used, the same, unmodified map
 -- will be saved
-loadCache :: IO (TVar DurationCacheMap)
+loadCache :: IO (TVar DurationCache)
 loadCache = do
   exists <- doesFileExist cacheFile
-  cache <- if exists
+  durationCache <- if exists
     -- TODO support streaming?
     then M.fromList . fmap (read . T.unpack) . T.lines <$> T.readFile cacheFile
     else pure M.empty
-  newTVarIO cache
+  newTVarIO $ DurationCache { durationCache, anyInserts = mempty }
 
-saveCache :: TVar DurationCacheMap -> IO ()
+-- | Saves the cache to file only if there were any changes (currently, even if
+-- an insert hasn't changed any contents).
+saveCache :: TVar DurationCache -> IO ()
 saveCache cacheVar = do
-  cache <- readTVarIO cacheVar
-  -- TODO support streaming?
-  -- FIXME use a better serialization approach
-  T.writeFile cacheFile . T.unlines . fmap (T.pack . show) . M.toList $ cache
+  DurationCache { durationCache, anyInserts } <- readTVarIO cacheVar
+  when (getAny anyInserts) .
+    -- TODO support streaming?
+    -- FIXME use a better serialization approach
+    T.writeFile cacheFile . T.unlines . fmap (T.pack . show) . M.toList $ durationCache
 
 -- FIXME use the cache location
 cacheFile :: FilePath
