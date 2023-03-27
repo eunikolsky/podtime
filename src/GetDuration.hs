8ced5dc54be0877acb938a5556b9d281e3d853a1
Eugene <e@egeek.me>
2023-03-27 10:28:33 +0300

    Cache calculated duration

--- src/GetDuration.hs
+++ src/GetDuration.hs
@@ -4,15 +4,17 @@ module GetDuration
   , getDuration
   ) where
 
-import Data.Maybe
+import Data.Functor
 import MP3
 
 -- | Provides a function to calculate duration of an MP3 file.
 class Monad m => MonadDuration m where
   calculateDuration :: FilePath -> m AudioDuration
 
+-- | Interface to access the audio durations cache.
 class Monad m => MonadDurationCache m where
   getCachedDuration :: FilePath -> m (Maybe AudioDuration)
+  cacheDuration :: FilePath -> AudioDuration -> m ()
 
 -- | Gets duration of an MP3 file, which may be retrieved from cache.
 getDuration :: (MonadDuration m, MonadDurationCache m) => FilePath -> m AudioDuration
@@ -21,4 +23,9 @@ getDuration fp = do
   -- note: even though the code may not look like it, but duration is calculated
   -- only if there is no cached duration
   duration <- calculateDuration fp
-  pure $ fromMaybe duration cached
+  maybe
+    -- if there is no cached value, cache it and return
+    (cacheDuration fp duration $> duration)
+    -- else return it w/o caching again
+    pure
+    cached
--- test/GetDurationSpec.hs
+++ test/GetDurationSpec.hs
@@ -1,6 +1,7 @@
 module GetDurationSpec (spec) where
 
 import Control.Monad.Reader
+import Control.Monad.State.Strict
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
 import GetDuration
@@ -12,11 +13,19 @@ import Test.QuickCheck
 spec :: Spec
 spec =
   describe "getDuration" $ do
-    prop "returns calculated duration" $
-      \(PrintableString fp) (PositiveAudioDuration duration) -> do
-        let durations = M.singleton fp duration
-            cachedDurations = CachedDurationMap M.empty
-        runTestDurationM (getDuration fp) durations cachedDurations `shouldBe` duration
+    context "when cached duration is missing" $ do
+      prop "returns calculated duration" $
+        \(PrintableString fp) (PositiveAudioDuration duration) -> do
+          let durations = M.singleton fp duration
+              cachedDurations = CachedDurationMap M.empty
+          runTestDurationM (getDuration fp) durations cachedDurations `shouldBe` duration
+
+      prop "caches calculated duration" $
+        \(PrintableString fp) (PositiveAudioDuration duration) -> do
+          let durations = M.singleton fp duration
+              cachedDurations = CachedDurationMap M.empty
+              expectedCachedDurations = CachedDurationMap durations
+          execTestDurationM (getDuration fp) durations cachedDurations `shouldBe` expectedCachedDurations
 
     context "when cached duration is present" $ do
       prop "returns cached duration" $
@@ -39,24 +48,30 @@ type DurationMap = Map FilePath AudioDuration
 
 -- | Map from a filename to its cached audio duration. It's used to mock a
 -- durations cache (`MonadDurationCache m`).
-newtype CachedDurationMap = CachedDurationMap DurationMap
+newtype CachedDurationMap = CachedDurationMap { getCachedDurationMap :: DurationMap }
+  deriving newtype (Show, Eq)
 
 -- | The monad to provide mock duration data in order to test `getDuration` in a
 -- pure way.
-newtype TestDurationM a = TestDurationM (Reader (DurationMap, CachedDurationMap) a)
-  deriving newtype (Functor, Applicative, Monad, MonadReader (DurationMap, CachedDurationMap))
+newtype TestDurationM a = TestDurationM (ReaderT DurationMap (State CachedDurationMap) a)
+  deriving newtype (Functor, Applicative, Monad, MonadReader DurationMap, MonadState CachedDurationMap)
 
 instance MonadDuration TestDurationM where
-  calculateDuration fp = asks $ M.findWithDefault 0 fp . fst
+  calculateDuration fp = asks $ M.findWithDefault 0 fp
 
 instance MonadDurationCache TestDurationM where
-  getCachedDuration fp = do
-    (CachedDurationMap durations) <- asks snd
-    pure $ M.lookup fp durations
+  getCachedDuration fp = gets $ M.lookup fp . getCachedDurationMap
+  cacheDuration fp duration = modify' $ CachedDurationMap . M.insert fp duration . getCachedDurationMap
 
--- | Runs the given action in the test monad with the provided mock data.
+-- | Runs the given action in the test monad with the provided mock data and
+-- returns the actions' result.
 runTestDurationM :: TestDurationM AudioDuration -> DurationMap -> CachedDurationMap -> AudioDuration
-runTestDurationM (TestDurationM a) durations cachedDurations = runReader a (durations, cachedDurations)
+runTestDurationM (TestDurationM a) durations cachedDurations = flip evalState cachedDurations $ runReaderT a durations
+
+-- | Runs the given action in the test monad with the provided mock data and
+-- returns the final cached durations.
+execTestDurationM :: TestDurationM AudioDuration -> DurationMap -> CachedDurationMap -> CachedDurationMap
+execTestDurationM (TestDurationM a) durations cachedDurations = flip execState cachedDurations $ runReaderT a durations
 
 newtype PositiveAudioDuration = PositiveAudioDuration AudioDuration
   deriving newtype (Show)
