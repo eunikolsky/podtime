657ee6481863afe40bf2276e1bfb344e61d4025d
Eugene <e@egeek.me>
2023-03-27 09:51:20 +0300

    Return cached duration if available

--- src/GetDuration.hs
+++ src/GetDuration.hs
@@ -1,14 +1,22 @@
 module GetDuration
   ( MonadDuration(..)
+  , MonadDurationCache(..)
   , getDuration
   ) where
 
+import Data.Maybe
 import MP3
 
 -- | Provides a function to calculate duration of an MP3 file.
 class Monad m => MonadDuration m where
   calculateDuration :: FilePath -> m AudioDuration
 
+class Monad m => MonadDurationCache m where
+  getCachedDuration :: FilePath -> m (Maybe AudioDuration)
+
 -- | Gets duration of an MP3 file, which may be retrieved from cache.
-getDuration :: MonadDuration m => FilePath -> m AudioDuration
-getDuration = calculateDuration
+getDuration :: (MonadDuration m, MonadDurationCache m) => FilePath -> m AudioDuration
+getDuration fp = do
+  cached <- getCachedDuration fp
+  duration <- calculateDuration fp
+  pure $ fromMaybe duration cached
--- test/GetDurationSpec.hs
+++ test/GetDurationSpec.hs
@@ -15,16 +15,39 @@ spec =
     prop "returns calculated duration" $
       \(PrintableString fp) (PositiveAudioDuration duration) -> do
         let durations = M.singleton fp duration
-        runTestDurationM (getDuration fp) durations `shouldBe` duration
+            cachedDurations = CachedDurationMap M.empty
+        runTestDurationM (getDuration fp) durations cachedDurations `shouldBe` duration
 
-newtype TestDurationM a = TestDurationM (Reader (Map FilePath AudioDuration) a)
-  deriving newtype (Functor, Applicative, Monad, MonadReader (Map FilePath AudioDuration))
+    prop "returns cached duration" $
+      \(PrintableString fp) (PositiveAudioDuration duration) -> do
+        let durations = M.empty
+            cachedDurations = CachedDurationMap $ M.singleton fp duration
+        runTestDurationM (getDuration fp) durations cachedDurations `shouldBe` duration
+
+-- | Map from a filename to its audio duration. It's used to mock the duration
+-- calculation instead of real parsing (`MonadDuration m`).
+type DurationMap = Map FilePath AudioDuration
+
+-- | Map from a filename to its cached audio duration. It's used to mock a
+-- durations cache (`MonadDurationCache m`).
+newtype CachedDurationMap = CachedDurationMap DurationMap
+
+-- | The monad to provide mock duration data in order to test `getDuration` in a
+-- pure way.
+newtype TestDurationM a = TestDurationM (Reader (DurationMap, CachedDurationMap) a)
+  deriving newtype (Functor, Applicative, Monad, MonadReader (DurationMap, CachedDurationMap))
 
 instance MonadDuration TestDurationM where
-  calculateDuration fp = asks $ M.findWithDefault 0 fp
+  calculateDuration fp = asks $ M.findWithDefault 0 fp . fst
 
-runTestDurationM :: TestDurationM AudioDuration -> Map FilePath AudioDuration -> AudioDuration
-runTestDurationM (TestDurationM a) = runReader a
+instance MonadDurationCache TestDurationM where
+  getCachedDuration fp = do
+    (CachedDurationMap durations) <- asks snd
+    pure $ M.lookup fp durations
+
+-- | Runs the given action in the test monad with the provided mock data.
+runTestDurationM :: TestDurationM AudioDuration -> DurationMap -> CachedDurationMap -> AudioDuration
+runTestDurationM (TestDurationM a) durations cachedDurations = runReader a (durations, cachedDurations)
 
 newtype PositiveAudioDuration = PositiveAudioDuration AudioDuration
   deriving newtype (Show)
